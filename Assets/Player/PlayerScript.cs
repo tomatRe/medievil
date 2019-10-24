f4d0b582e9a1f8d9207ded902695c2d853a92967
tomatRe <angelre.rebollo@gmail.com>
2019-10-24 21:09:58 +0200

    Player Updated

--- Assets/Player/PlayerScript.cs
+++ Assets/Player/PlayerScript.cs
@@ -6,8 +6,8 @@ public class PlayerScript : MonoBehaviour
 {
 
     //[SerializeField] private float cameraSensitivity = 2;
-    [SerializeField] private float speed = 1;
-    [SerializeField] private float jumpSpeed = 10;
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float jumpSpeed = 100;
     [SerializeField] private Transform model;
     [SerializeField] private new Camera camera;
     private Animation anim;
@@ -30,13 +30,27 @@ public class PlayerScript : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         float jump = Input.GetAxis("Jump");
 
+        Vector3 moveVector = new Vector3(transform.position.x * horizontal,
+            0f, (transform.position.z - camera.transform.position.z) * vertical);
+
+        GetComponent<Rigidbody>().AddForce(moveVector * speed * Time.deltaTime, ForceMode.Acceleration);
+
+        Debug.Log(moveVector);
+
+        /*
         Vector3 cameraVector = camera.transform.forward;
 
         GetComponent<Rigidbody>().velocity = 
             new Vector3(
-                vertical * (speed * cameraVector.x) + horizontal * (speed * cameraVector.z),
+                vertical * (speed * cameraVector.x * Time.deltaTime), 0, 0);
+
+        GetComponent<Rigidbody>().velocity =
+            new Vector3(
+                horizontal * (speed * cameraVector.z),
                 0,
-                vertical * (speed * cameraVector.z) + horizontal * (speed * cameraVector.x));
+                horizontal * (speed * cameraVector.x));*/
+
+
 
         if (jump > 0)
         {
@@ -49,27 +63,22 @@ public class PlayerScript : MonoBehaviour
 
     void AnimateMovement(float horizontal, float vertical)
     {
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
         if (horizontal > 0.1f || horizontal < -0.1f || vertical > 0.1f || vertical < -0.1f)
         {
             anim.Play("run");
 
-            if (horizontal > 0.1f)//Derecha
+            if (camera.transform.rotation.y > 90 || camera.transform.rotation.y < -90)
             {
-                model.rotation = new Quaternion(0,1,0,1);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-movement), 0.15F);
             }
-            if (horizontal < -0.1f)//Izquierda
+            else
             {
-                model.rotation = new Quaternion(0, -1, 0, 1);
-            }
-            if (vertical > 0.1f)//Arriba
-            {
-                model.rotation = new Quaternion(0, 1, 0, 0);
-            }
-            if (vertical < -0.1f)//Abajo
-            {
-                model.rotation = new Quaternion(0, -1, 0, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
             }
 
+            
         }
         else
         {

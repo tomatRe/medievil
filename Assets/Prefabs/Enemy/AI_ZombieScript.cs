a306f28912223eb2f9589ae2037a5cd528c61556
Angel Rebollo <angelre.rebollo@gmail.com>
2019-10-25 14:27:04 +0200

    Improvements on the zombie

--- Assets/Prefabs/Enemy/AI_ZombieScript.cs
+++ Assets/Prefabs/Enemy/AI_ZombieScript.cs
@@ -8,7 +8,12 @@ public class AI_ZombieScript : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private new Rigidbody rigidbody;
     private NavMeshAgent ai;
-    
+    public float speed = 5;
+    public float directionChangeInterval = 1;
+    public float maxHeadingChange = 30;
+    float heading;
+    Vector3 targetRotation;
+
 
     void Awake()
     {
@@ -27,13 +32,48 @@ public class AI_ZombieScript : MonoBehaviour
 
     }
 
-    void FixedUpdate()
+    private void Start()
     {
-        Move();
+        heading = Random.Range(0, 360);
+        transform.eulerAngles = new Vector3(0, heading, 0);
+
+        StartCoroutine(NewHeading());
+    }
+
+    private void Update()
+    {
+        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+        var forward = transform.TransformDirection(Vector3.forward);
+
+        GetComponent<Rigidbody>().velocity = forward*speed*Time.deltaTime;
+
+
+        if (GetComponent<Rigidbody>().velocity.magnitude > 0.1f)
+        {
+            
+        }
+    }
+
+    IEnumerator NewHeading()
+    {
+        while (true)
+        {
+            NewHeadingRoutine();
+            yield return new WaitForSeconds(directionChangeInterval);
+        }
+    }
+
+
+    void NewHeadingRoutine()
+    {
+        var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+        var ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+        heading = Random.Range(floor, ceil);
+        targetRotation = new Vector3(0, heading, 0);
     }
 
     void Move()
     {
-        
+        //ai.SetDestination();
     }
 }
